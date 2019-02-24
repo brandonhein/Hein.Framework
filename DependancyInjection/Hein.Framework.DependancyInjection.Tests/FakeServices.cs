4eb5a0eadbed9f1bf76bbe520947a68e46850f26
Brandon Hein <bhein@lssu.edu>
2019-02-24 10:32:18 -0500

    dependancy injection tests

---
+++ DependancyInjection/Hein.Framework.DependancyInjection.Tests/FakeServices.cs
@@ -0,0 +1,47 @@
+﻿namespace Hein.Framework.DependancyInjection.Tests
+{
+    public class FakeServiceOne : IFakeServiceOne
+    {
+        public decimal Calculate()
+        {
+            return 42m;
+        }
+    }
+
+    public interface IFakeServiceOne
+    {
+        decimal Calculate();
+    }
+
+    public class FakeServiceTwo : IFakeServiceTwo
+    {
+        private readonly string _name;
+        public FakeServiceTwo(string name)
+        {
+            _name = name;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+    }
+
+    public interface IFakeServiceTwo
+    {
+        string GetName();
+    }
+
+    public class FakeServiceThree : IFakeServiceThree
+    {
+        public void Execute()
+        {
+            //https://www.youtube.com/watch?v=4zLfCnGVeL4
+        }
+    }
+
+    public interface IFakeServiceThree
+    {
+        void Execute();
+    }
+}
