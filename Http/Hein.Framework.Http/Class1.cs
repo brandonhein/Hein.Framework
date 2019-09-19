5adffea7848ebd0b0c268b276dded19843538cdd
Hein, Brandon <BrandonHein@quickenloans.com>
2019-09-19 17:37:23 -0400

    sync

--- Http/Hein.Framework.Http/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Hein.Framework.Http
-{
-    public class Class1
-    {
-    }
-}
---
+++ Http/Hein.Framework.Http/HttpContentType.cs
@@ -0,0 +1,11 @@
+﻿namespace Hein.Framework.Http
+{
+    public static class HttpContentType
+    {
+        public const string Json = "application/json";
+        public const string Xml = "application/xml";
+        public const string TextXml = "text/xml";
+        public const string Text = "text/plain";
+        public const string UrlEncode = "application/x-www-form-urlencoded";
+    }
+}
