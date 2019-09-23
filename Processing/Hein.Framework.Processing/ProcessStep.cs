9dce886869a9166442d51a177e377cdca111a2f2
Hein, Brandon <BrandonHein@quickenloans.com>
2019-09-23 09:43:16 -0400

    processing package

---
+++ Processing/Hein.Framework.Processing/ProcessStep.cs
@@ -0,0 +1,21 @@
+﻿namespace Hein.Framework.Processing
+{
+    public abstract class ProcessStep<T> where T : IProcessContext
+    {
+        public string StepName { get; }
+        protected ProcessStep(string stepName)
+        {
+            StepName = stepName;
+        }
+
+        /// <summary>
+        /// Runs validation on the processcontext to determine if the current step should run or not
+        /// </summary>
+        public abstract bool ShouldExecute(T context);
+
+        /// <summary>
+        /// Runs/applies logic to the processcontext.  if it returns 'true' it will proceed to the next step. if 'false' it breaks out of the execution of steps
+        /// </summary>
+        public abstract bool Execute(T context);
+    }
+}
