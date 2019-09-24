d92b74769ad4b84fa3bfc7a4261d5e069d3e53f9
Hein, Brandon <BrandonHein@quickenloans.com>
2019-09-24 10:44:44 -0400

    adding a sample web api application

---
+++ Identity/Hein.Framework.Identity.Sample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hein.Framework.Identity.Sample.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var name = new GetUserNameService().GetName();
+            return Ok(new { userName = name });
+        }
+    }
+
+    public class GetUserNameService
+    {
+        public string GetName()
+        {
+            //use Hein.Framework.Identity.HttpContext to get user
+            return HttpContext.Current.User.Identity.Name;
+        }
+    }
+}
---
+++ Identity/Hein.Framework.Identity.Sample/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace Hein.Framework.Identity.Sample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseWindowsAuthentication(); //<-- enable windows auth as a webhost
+    }
+}
