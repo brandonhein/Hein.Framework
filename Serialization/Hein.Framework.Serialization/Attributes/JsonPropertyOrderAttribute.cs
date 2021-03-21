46dd7755427a3d3dd898d405ec3543c424aa509c
Brandon Hein <brandonhein19@gmail.com>
2021-03-21 10:55:46 -0400

    trying to figure out JsonProperty Ordering

---
+++ Serialization/Hein.Framework.Serialization/Attributes/JsonPropertyOrderAttribute.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//using System.Text.Json.Serialization;
+//
+//namespace Hein.Framework.Serialization.Attributes
+//{
+//    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+//    public sealed class JsonPropertyOrderAttribute : JsonAttribute
+//    {
+//        public JsonPropertyOrderAttribute(int order)
+//        {
+//            Order = order;
+//        }
+//    
+//        public int Order { get; }
+//    }
+//}
