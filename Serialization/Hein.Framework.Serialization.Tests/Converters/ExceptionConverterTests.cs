45167cb1238fe47de8f6d3ad82966442fbda6083
Brandon Hein <brandonhein19@gmail.com>
2021-04-02 17:11:02 -0400

    converters

---
+++ Serialization/Hein.Framework.Serialization.Tests/Converters/ExceptionConverterTests.cs
@@ -0,0 +1,18 @@
+﻿//using Hein.Framework.Serialization.Converters;
+//using System.Net;
+//using System.Net.Mail;
+//using Xunit;
+//
+//namespace Hein.Framework.Serialization.Tests.Converters
+//{
+//    public class ExceptionConverterTests
+//    {
+//        [Fact]
+//        public void Should_serialize_exceptions_and_an_okay_fashion()
+//        {
+//            var innerException = new WebException("message", WebExceptionStatus.ProtocolError);
+//            var exception = new SmtpException("ope", innerException);
+//            var json = exception.ToJson(new ExceptionConverter());
+//        }
+//    }
+//}
---
+++ Serialization/Hein.Framework.Serialization/Converters/ByteArrayConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Hein.Framework.Serialization.Converters
+{
+    public class ByteArrayConverter : JsonConverter<byte[]>
+    {
+        public override bool CanConvert(Type typeToConvert) => typeToConvert == typeof(byte[]) || typeToConvert == typeof(IEnumerable<byte>);
+
+        public override byte[] Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String && reader.TryGetBytesFromBase64(out var bytes))
+            {
+                return bytes;
+            }
+
+            return default(byte[]);
+        }
+
+        public override void Write(Utf8JsonWriter writer, byte[] value, JsonSerializerOptions options)
+        {
+            writer.WriteBase64StringValue(value);
+        }
+    }
+}
