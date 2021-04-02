e60cd6a273b69d06ab3ac89b03d3ac43ca8e54dd
Brandon Hein <brandonhein19@gmail.com>
2021-04-02 17:08:02 -0400

    adding some common json naming policies... like snake_case

---
+++ Serialization/Hein.Framework.Serialization/Policies/LowerCaseNamingPolicy.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json;
+
+namespace Hein.Framework.Serialization.Policies
+{
+    public class LowerCaseNamingPolicy : JsonNamingPolicy
+    {
+        public override string ConvertName(string name) => name.ToLower();
+    }
+}
