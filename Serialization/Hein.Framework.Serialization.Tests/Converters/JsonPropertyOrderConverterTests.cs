4caa0b59d3ab450effa6e020cb7d8b75d0c0db63
Brandon Hein <brandonhein19@gmail.com>
2021-04-02 17:08:44 -0400

    JsonPropertyOrder Attribute and Converter... awesomeness

---
+++ Serialization/Hein.Framework.Serialization.Tests/Converters/JsonPropertyOrderConverterTests.cs
@@ -0,0 +1,47 @@
+﻿using Hein.Framework.Serialization.Attributes;
+using System.Text.Json.Serialization;
+using Xunit;
+
+namespace Hein.Framework.Serialization.Tests.Converters
+{
+    public class JsonPropertyOrderConverterTests
+    {
+        [Fact]
+        public void Should_order_json_properties_correctly_due_to_order_attribute()
+        {
+            var sample = new SampleOrderClass() { First = "first", Middle = "middle", Last = "last" };
+
+            var json = sample.ToJson();
+            var expectedJson = "{\"FIRST\":\"first\",\"Middle\":\"middle\",\"last_value\":\"last\"}";
+            Assert.Equal(expectedJson, json);
+        }
+
+        [Fact]
+        public void Should_deserialize_json_correctly_order_doesnt_matter_here_but_do_name_check()
+        {
+            var json = "{\"FiRsT\":\"lightning\",\"miDDle\":\"thunder\",\"last_VALUE\":\"rain\"}";
+
+            var sample = json.FromJson<SampleOrderClass>();
+
+            Assert.Equal("lightning", sample.First);
+            Assert.Equal("thunder", sample.Middle);
+            Assert.Equal("rain", sample.Last);
+        }
+    }
+
+    public class SampleOrderClass : SampleOrderClassBase
+    {
+        [JsonPropertyOrder(3)]
+        [JsonPropertyName("last_value")]
+        public string Last { get; set; }
+        [JsonPropertyOrder(2)]
+        public string Middle { get; set; }
+    }
+
+    public class SampleOrderClassBase
+    {
+        [JsonPropertyOrder(1)]
+        [JsonPropertyName("FIRST")]
+        public string First { get; set; }
+    }
+}
--- Serialization/Hein.Framework.Serialization/Attributes/JsonPropertyOrderAttribute.cs
+++ Serialization/Hein.Framework.Serialization/Attributes/JsonPropertyOrderAttribute.cs
@@ -1,16 +1,19 @@
-﻿//using System;
-//using System.Text.Json.Serialization;
-//
-//namespace Hein.Framework.Serialization.Attributes
-//{
-//    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
-//    public sealed class JsonPropertyOrderAttribute : JsonAttribute
-//    {
-//        public JsonPropertyOrderAttribute(int order)
-//        {
-//            Order = order;
-//        }
-//    
-//        public int Order { get; }
-//    }
-//}
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Hein.Framework.Serialization.Attributes
+{
+    /// <summary>
+    /// Orders a property to be in a specific order when serailizing
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public sealed class JsonPropertyOrderAttribute : JsonAttribute
+    {
+        public JsonPropertyOrderAttribute(int order)
+        {
+            Order = order;
+        }
+    
+        public int Order { get; }
+    }
+}
